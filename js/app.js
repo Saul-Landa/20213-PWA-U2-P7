e58dd28047018e4994840c01be87e5716c9891a1
Saúl Landa <saul.landa.2000@gmail.com>
2021-11-14 03:38:23 -0600

    Saúl Landa: Fucionalidad principaSaúl Landa: Fucionalidad principal

---
+++ js/app.js
@@ -0,0 +1,56 @@
+if (navigator.serviceWorker) {
+    let url = ''
+    const BASE_URL = window.location.href
+    BASE_URL.startsWith('https:') ? url = '/20213-PWA-U2-P7/sw.js' : url = "/sw.js"
+    navigator.serviceWorker.register(url)
+}
+
+let player = $("#player");
+let photoUser = $("#fotoUser");
+
+let btnCamera = $("#btnCamara");
+let btnCameraBack = $("#btnCamaraBack");
+let btnTakePhoto = $("#btnCamaraBack");
+
+const camera = new Camera(player[0]);
+
+btnCamera.on("click", () => {
+    camera.on().then((result) => {
+        if (!result) {
+            Swal.fire({
+                icon: 'error',
+                title: 'Oops...',
+                text: 'Verifica que la cámara este encendia',
+            })
+        }
+    });
+});
+
+btnCameraBack.on("click", () => {
+    console.log("camara back");
+    camera.onBack().then((result) => {
+        if (!result) {
+            Swal.fire({
+                icon: 'error',
+                title: 'Oops...',
+                text: 'Verifica que la cámara este encendia',
+            })
+        }
+    });
+});
+
+btnTakePhoto.on("click", async () => {
+    camera.off();
+    const [foto, tipo] = camera.takePhoto();
+    const card = `
+        <div class="m-2 card mt-5">
+                <img class="card-img-top"
+                    src="${foto}"
+                    alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">${tipo}</h5>
+                </div>
+            </div>
+        `;
+    $("#photoContainer").append(card);
+});
